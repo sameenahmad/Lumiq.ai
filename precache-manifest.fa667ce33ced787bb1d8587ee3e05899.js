f80c376662bfe3101aa6798969bd38cdaac18edc
Sameen Ahmad <sameen.ahmad21@gmail.com>
2021-03-02 01:56:16 +0530

    Updates

---
+++ precache-manifest.fa667ce33ced787bb1d8587ee3e05899.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a271b2e5e974fe4b45edbcf1e12c3f",
+    "url": "/Lumiq.ai/index.html"
+  },
+  {
+    "revision": "a9dffad76cce735d0185",
+    "url": "/Lumiq.ai/static/css/main.b7167556.chunk.css"
+  },
+  {
+    "revision": "57620889a5396c429b5b",
+    "url": "/Lumiq.ai/static/js/2.982cb2f7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Lumiq.ai/static/js/2.982cb2f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9dffad76cce735d0185",
+    "url": "/Lumiq.ai/static/js/main.281cd9a7.chunk.js"
+  },
+  {
+    "revision": "8ca9b044c4cc171279e9",
+    "url": "/Lumiq.ai/static/js/runtime-main.ff85885b.js"
+  },
+  {
+    "revision": "d8a7d7cdcc3e2d739f6f3607f1dc77a6",
+    "url": "/Lumiq.ai/static/media/pattern.d8a7d7cd.svg"
+  }
+]);
